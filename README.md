800f4a09892429738f699ced348e2300d672fb97
VINICIUS <valverdevinicius@protonmail.com>
2023-06-22 17:28:52 -0300

    All commits

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jogo-de-Xadrez
+Jogo de Xadrez no terminal, porém eficiente!
+
+Jogo completamente escrito em Java aplicando diversos tipos de conhecimentos da linguagem.
+
+Nativamente, o jogo não funciona no cmd, podendo ser rodado no terminal Linux, Git Bash entre outros
---
+++ src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		ChessMatch cm = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
+		while (!cm.getCheckMate()) {
+			try {
+				UI.clearScreen();
+				UI.printMatch(cm, captured);
+				System.out.println();
+				System.out.print("Enter the SOURCE position: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				boolean[][] possibleMoves = cm.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(cm.getPieces(), possibleMoves);
+				System.out.println();
+				System.out.println();
+				System.out.print("Enter the TARGET position: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = cm.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				if (cm.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						sc.nextLine().toUpperCase();
+					}
+					cm.replacePromotedPiece(type);
+				}
+				
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+		}
+		UI.clearScreen();
+		UI.printMatch(cm, captured);
+	}
+}
